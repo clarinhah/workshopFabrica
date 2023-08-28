b2f36e1625cc0bbfe874ae4d6ef2c72340206b41
clarinhah <clariceluiza1718@gmail.com>
2023-08-28 17:04:15 -0300

    Enviando primeira atividade

---
+++ atividade01.py
@@ -0,0 +1,7 @@
+numero = input("Digite um número:")
+
+sucessor = int(numero) + 1 
+antecessor =int(numero) - 1
+
+print("O sucessor do núemro é:",  sucessor)
+print("O antecessor do núemro é:", antecessor)
